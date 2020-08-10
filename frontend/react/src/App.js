ea9d62e46d18e9a712e6dd688407cf46af8edc51
Seth Sacher <ssacher@collabralink.com>
2020-08-10 14:12:16 -0400

    Log user info

--- frontend/react/src/App.js
+++ frontend/react/src/App.js
@@ -16,12 +16,25 @@ function App() {
   let VisibleFooter =
     window.location.pathname.split("/")[1] === "reports" ? null : <Footer />;
 
+    Auth.currentUserInfo().then(res=> {
+      console.log(res)
+    })
+
   Auth.currentSession().then(res=>{
+    console.log(res)
     let accessToken = res.getAccessToken()
     let jwt = accessToken.getJwtToken()
     //You can print them to see the full objects
     console.log(`myAccessToken: ${JSON.stringify(accessToken)}`)
     console.log(`myJwt: ${jwt}`)
+    // fetch("https://cmscartsseds3e4e2271-3e4e2271-dev.auth.us-east-1.amazoncognito.com/oauth2/userInfo", {
+    //   method: "GET",
+    //   headers: {
+    //     "Authorization": `Bearer ${accessToken}`
+    //   }
+    // })
+    //   .then(response => response.json())
+    //   .then(data => console.log(data));
   });
 
   return (
